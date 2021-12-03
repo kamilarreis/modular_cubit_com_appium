ad9029b2e2fa3b56a564e9a503c35427d14ac974
JoaoMendonca11 <kamilabasil@yahoo.com.br>
2021-12-03 15:25:21 -0300

    Atualização de dependencias

--- app/test/pages/domain/usecases/login_use_case_test.dart
+++ app/test/pages/domain/usecases/login_use_case_test.dart
@@ -3,7 +3,6 @@
 import 'package:app/pages/login/domain/entities/login_entitie.dart';
 import 'package:app/pages/login/domain/repositories/login_repository.dart';
 import 'package:app/pages/login/domain/usecases/login_use_case.dart';
-import 'package:app/pages/login/external/mock/profileMock.dart';
 import 'package:app/pages/profile/domain/entities/profile_entitie.dart';
 import 'package:app/pages/util/errors/erros_util.dart';
 import 'package:dartz/dartz.dart';
--- app/test/pages/presenter/cubit/login_cubit_test.dart
+++ app/test/pages/presenter/cubit/login_cubit_test.dart
@@ -8,41 +8,46 @@ import 'package:app/pages/profile/domain/entities/profile_entitie.dart';
 import 'package:app/pages/profile/presenter/cubit/profile_state.dart';
 import 'package:app/pages/util/errors/erros_util.dart';
 import 'package:dartz/dartz.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
+
 //Mock do repositorio
-class MockLoginRepository extends Mock implements ILoginRepository{}
+class MockLoginRepository extends Mock implements ILoginRepository {}
 
 void main() {
-
   late LoginUseCaseImpl useCase;
   late ILoginRepository repository;
   late LoginCubit cubit;
-  LoginController loginController;
+
 
   setUp(() {
+      repository = MockLoginRepository();
+      useCase = LoginUseCaseImpl(repository);
+      final loginController = LoginController(useCase);
+      cubit = LoginCubit(useCase, loginController);
+    });
 
-    AppModule();
-    loginController = Modular.get<LoginController>();
-    repository = MockLoginRepository();
-    useCase = LoginUseCaseImpl(repository);
-    cubit = LoginCubit(useCase, loginController);
+    Login login = Login(email: 'teste@teste.com', password: '123456');
+    final profile = Profile(email: 'test@gmail.com', name: 'Usuario de teste', profissao: 'Desenvolvedor');
 
-  });
-  Login login = Login(email: 'teste@teste.com', password: '123456');
-  final profile =  Profile(email: 'test@gmail.com', name: 'Usuario de teste', profissao: 'Desenvolvedor');
-
-  test('Deve verificar o stado depois do login', () async {
+  test('Deve verificar o estado depois do login',()async {
     when(() => repository.getLogin(login)).thenAnswer((_) async => Right<Failure, Profile>(profile));
     final result = await useCase(login);
-    expect(result, isNotEmpty);
-    expect(cubit.state, equals(LoggedState()));
-    print(cubit.state.toString());
+    expect(result, equals(Right<Failure, Profile> (profile)));
+    print(result.toString());
   });
 
-  tearDown(() {
-    print('Fim do teste');
-  });
-}
+    test('Deve verificar o estado depois do login', () async {
+      when(() => repository.getLogin(login)).thenAnswer((_) async => Right<Failure, Profile>(profile));
+      final result = await useCase(login);
+      expect(result, equals(Right<Failure, Profile> (profile)));
+      expect(cubit.state, equals(LoggedState()));
+
+    });
+
+    tearDown(() {
+      print('Fim do teste');
+    });
+  }
+
